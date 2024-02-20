f9be4ad98c00ddb6d672c3ee44c777704ea2c772
Abrorbek-Ibaydullaev <148473615+Abrorbek-Ibaydullaev@users.noreply.github.com>
2024-02-20 15:17:08 +0100

    Update script.js

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -11,3 +11,22 @@ const nav = document.querySelector(".nav"),
     totalNavList = navList.length,
     allSection = document.querySelectorAll(".section"),
     totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        removeBackSection();
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
+                // allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+        if(window.innerWidth < 1200)
+        {
+            asideSectionTogglerBtn();
+        }
+    })
+}
