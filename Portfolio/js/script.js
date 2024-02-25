1798706d5082cd954924a3c2c29ee35e2283551a
Abrorbek-Ibaydullaev <148473615+Abrorbek-Ibaydullaev@users.noreply.github.com>
2024-02-25 21:28:00 +0100

    Update script.js

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -41,3 +41,45 @@ function addBackSection(num)
 {
     allSection[num].classList.add("back-section");
 }
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
+}
+function updateNav(element)
+{
+    for(let i = 0; i < totalNavList; i++)
+    {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+        {
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+document.querySelector(".hire-me").addEventListener("click", function()
+{
+    const sectionIndex = this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+    navTogglerBtn.addEventListener("click",() => 
+    {
+        asideSectionTogglerBtn()
+    })
+    function asideSectionTogglerBtn()
+    {
+        aside.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.toggle("open");
+        }
+    }
